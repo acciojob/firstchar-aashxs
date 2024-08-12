4d3ad07093312d1967221e7486d575f8ccb3b762
Ashish <90548985+aashxs@users.noreply.github.com>
2024-08-12 13:57:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
 function firstChar(text) {
   // your code here
-	if((text.length==0) || (text.trim().length==0) ){
-		let str="";
-		return str;
-	}
-	  else{
-		  let l=text[0];
-	return l;
+	for(let i = 0; i < text.length; i++) {
+   
+    if(text[i] !== ' ') {
+      
+      return text[i];
     }
+  }
+ 
+  return '';
 }
 
 // Do not change the code below
